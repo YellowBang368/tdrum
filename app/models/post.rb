82a84d8bb52f6a85627fc825d787d5afef4b38aa
TopClone <yellowbang368@gmail.com>
2018-11-04 20:19:20 +0300

    Fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   mount_uploaders :images, ImageUploader
+
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -30,19 +30,4 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
-  config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
-    new
-    export
-    bulk_delete
-    show
-    edit
-    delete
-    show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
-  end
 end
